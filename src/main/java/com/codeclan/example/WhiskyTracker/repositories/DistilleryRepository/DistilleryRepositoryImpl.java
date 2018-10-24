a30e19c8dd99fac7bfcaedfc81a169be2908270c
self-unit <self_unit@hotmail.com>
2018-10-24 15:34:24 +0100

    fist commit

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+
+public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
+
+    //TODO: create a method to find all whiskeys of a particular year
+    //TODO: create a method to find all whiskeys of a particular region
+    //TODO: create a method to find all distilleries of a particular region
+    //TODO: create a method to find all whiskeys of a particular distillery with a specific age
+    //TODO: create a method to find all distilleries that have whiskey of a particular age
+}
