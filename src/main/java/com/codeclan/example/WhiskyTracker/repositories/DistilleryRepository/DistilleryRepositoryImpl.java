4a1cb1cf1511c9ce7a51668a0ed8c42ae4704f69
self-unit <self_unit@hotmail.com>
2018-10-24 15:43:17 +0100

    ignore files

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
@@ -1,5 +1,12 @@
 package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
 
     //TODO: create a method to find all whiskeys of a particular year
@@ -7,4 +14,12 @@ public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
     //TODO: create a method to find all distilleries of a particular region
     //TODO: create a method to find all whiskeys of a particular distillery with a specific age
     //TODO: create a method to find all distilleries that have whiskey of a particular age
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Distillery> getDistilleriesWithParticularWhiskeyAge(){
+
+    }
 }
